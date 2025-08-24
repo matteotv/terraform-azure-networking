7530fe5f62c681929af57ff722fd238c99fb53a5
matteotv <matteotv.it@gmail.com>
2025-08-24 19:52:17 +0100

    feat: update output values

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 # Outputs
 output "virtual_network_id" {
-  value = azurerm_virtual_network.this.id
+  value = azurerm_virtual_network.this
 }
 
-output "subnet_ids" {
-  value = { for name, subnet in azurerm_subnet.this : name => subnet.id }
+output "subnets" {
+  value = { for name, subnet in azurerm_subnet.this : name => subnet }
 }
 
-output "network_security_group_ids" {
-  value = var.create_security_groups ? { for name, nsg in azurerm_network_security_group.this : name => nsg.id } : {}
+output "network_security_groups" {
+  value = var.create_security_groups ? { for name, nsg in azurerm_network_security_group.this : name => nsg } : {}
 }
 
-output "route_table_ids" {
-  value = var.create_route_tables ? { for name, rt in azurerm_route_table.this : name => rt.id } : {}
+output "route_tables" {
+  value = var.create_route_tables ? { for name, rt in azurerm_route_table.this : name => rt } : {}
 }
